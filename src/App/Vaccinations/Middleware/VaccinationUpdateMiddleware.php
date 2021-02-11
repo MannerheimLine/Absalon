68843d81057936570c7f4c2e93627c420d1e3046
Mikki <Valehtelit@mail.ru>
2021-02-11 19:58:59 +1000

    #58.1 Механизм обновления записи прививки Забыл миддлвар

---
+++ src/App/Vaccinations/Middleware/VaccinationUpdateMiddleware.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Absalon\Application\Vaccinations\Middleware;
+
+use Absalon\Application\Vaccinations\Domains\VaccinationFactory;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+
+class VaccinationUpdateMiddleware implements MiddlewareInterface
+{
+    /**
+     * Process an incoming server request.
+     *
+     * Processes an incoming server request in order to produce a response.
+     * If unable to produce the response itself, it may delegate to the provided
+     * request handler to do so.
+     */
+    public function process(ServerRequestInterface $request, RequestHandlerInterface $handler) : ResponseInterface
+    {
+        $dto = VaccinationFactory::create($request->getAttribute('ValidatedFields'));
+        $request = $request->withAttribute('DTO', $dto);
+        return $response = $handler->handle($request);
+    }
+}
